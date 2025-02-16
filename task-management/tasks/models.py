a65db39a3bcd670cb631c921c72515d875d92546
mashukabashar <diyamoni422@gmail.com>
2025-02-16 14:46:25 +0600

    module-5-complete

--- task-management/tasks/models.py
+++ task-management/tasks/models.py
@@ -17,15 +17,13 @@ class Task(models.Model):
     )
 
    
-    assigned_to = models.ManyToManyField(Employee)
-    # , related_name='tasks'
+    assigned_to = models.ManyToManyField(Employee, related_name='tasks')
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # details
 
 
 
@@ -41,8 +39,8 @@ class TaskDetail(models.Model):
     )
     task = models.OneToOneField(
         Task,
-        on_delete=models.CASCADE
-        # related_name='details'
+        on_delete=models.CASCADE,
+        related_name='details'
     )
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(
