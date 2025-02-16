14eb446a1b27c7702bc785f86b3472e7b993a919
mashukabashar <diyamoni422@gmail.com>
2025-02-16 20:33:21 +0600

    module-6-complete

--- task-management/tasks/models.py
+++ task-management/tasks/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
     
 
 
